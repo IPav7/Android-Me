24f408b82ec7fa2fbe0a917835c79e161c5c0a0c
IPav7 <pavinich1997@mail.ru>
2018-03-20 01:44:02 +0300

    exercise 1

---
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.android_me.ui;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.android_me.R;
+
+public class AndroidMeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_me);
+
+        BodyPartFragment bodyPartFragment = new BodyPartFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.head_container, bodyPartFragment)
+                .commit();
+    }
+}
