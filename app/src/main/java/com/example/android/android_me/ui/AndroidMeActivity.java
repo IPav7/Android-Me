3279083dd2fb5f9793e4d6208cdc3b99618570c0
IPav7 <pavinich1997@mail.ru>
2018-03-20 19:40:29 +0300

    communication between activities and fragments

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
@@ -14,23 +16,27 @@ public class AndroidMeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
+        Bundle bundle = getIntent().getBundleExtra(MainActivity.INDEXES);
+        int headIndex = bundle.getInt(MainActivity.HEAD_INDEX, 0);
+        int bodyIndex = bundle.getInt(MainActivity.BODY_INDEX, 0);
+        int legIndex = bundle.getInt(MainActivity.LEG_INDEX, 0);
         if(savedInstanceState == null) {
             BodyPartFragment headPartFragment = new BodyPartFragment();
             headPartFragment.setImageIDs(AndroidImageAssets.getHeads());
-            headPartFragment.setListIndex(0);
+            headPartFragment.setListIndex(headIndex);
             FragmentManager fragmentManager = getSupportFragmentManager();
             fragmentManager.beginTransaction()
                     .add(R.id.head_container, headPartFragment)
                     .commit();
             BodyPartFragment bodyPartFragment = new BodyPartFragment();
             bodyPartFragment.setImageIDs(AndroidImageAssets.getBodies());
-            bodyPartFragment.setListIndex(0);
+            bodyPartFragment.setListIndex(bodyIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.body_container, bodyPartFragment)
                     .commit();
             BodyPartFragment legPartFragment = new BodyPartFragment();
             legPartFragment.setImageIDs(AndroidImageAssets.getLegs());
-            legPartFragment.setListIndex(0);
+            legPartFragment.setListIndex(legIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.leg_container, legPartFragment)
                     .commit();
--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -1,21 +1,62 @@
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
 
+    public static final String HEAD_INDEX = "head_index";
+    public static final String BODY_INDEX = "body_index";
+    public static final String LEG_INDEX = "leg_index";
+    public static final String INDEXES = "indexes";
+
+    private int headIndex;
+    private int bodyIndex;
+    private int legIndex;
+
+    Intent mIntent;
+    Button mButton;
+    Bundle bundle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = findViewById(R.id.next_button);
+        mIntent = new Intent(this, AndroidMeActivity.class);
+        bundle = new Bundle();
+        mIntent.putExtra(INDEXES, bundle);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(mIntent);
+            }
+        });
     }
 
     @Override
     public void onImageSelected(int position) {
-        Toast.makeText(this, "Image N" + position + " clicked", Toast.LENGTH_SHORT).show();
+        switch (position/12){
+            case 0:
+                headIndex = position % 12;
+                bundle.putInt(HEAD_INDEX, headIndex);
+                break;
+            case 1:
+                bodyIndex = position % 12;
+                bundle.putInt(BODY_INDEX, bodyIndex);
+                break;
+            case 2:
+                legIndex = position % 12;
+                bundle.putInt(LEG_INDEX, legIndex);
+                break;
+            default: Toast.makeText(this, "Unknown image clicked " + (position/12), Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
